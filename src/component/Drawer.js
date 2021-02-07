8189b27907131a7f3fe50c88eeec862a1e10c493
Raffi Utama <raffifebriandikau@gmail.com>
2021-02-07 13:48:02 +0700

    add tailwind support

--- src/component/Drawer.js
+++ src/component/Drawer.js
@@ -1,19 +1,18 @@
 function Drawer() {
     return (
         <>
-            {/* Header */}
-            <div className="drawer" style={{ left: '20px', top: '20px', right: '20px', display: 'flex', justifyContent: 'space-between'}}>
+            <div className="header">
                 <div style={{ backgroundColor: 'white', width: '400px', height: '40px'}}></div>
                 <div style={{ backgroundColor: 'white', width: '300px', height: '40px'}}></div>
                 <div style={{ backgroundColor: 'white', width: '350px', height: '40px'}}></div>
             </div>
-            {/* Side Pane */}
-            <div className="drawer" style={{ left: '20px', top: '80px', width: '50px', height: '300px', backgroundColor: 'white' }}></div>
-            {/* Zoom */}
-            <div className="drawer" style={{ left: '20px', bottom: '20px', width: '50px', height: '100px', backgroundColor: 'white'}}></div>
-            {/* Avionic Indicator */}
-            <div className="drawer" style={{ right: '20px', bottom: '20px', display: 'flex', gap: '10px'}}>
-                <div style={{ display: 'grid', gridTemplateColumns: '1fr' }}>
+
+            <div className="side-pane" style={{ width: '50px', height: '300px', backgroundColor: 'white' }}></div>
+
+            <div className="map-control" style={{ width: '50px', height: '100px', backgroundColor: 'white'}}></div>
+
+            <div className="bottom-indicator">
+                <div className="avionic">
                     <div style={{ backgroundColor: 'white', width: '60px', height: '30px'}}></div>
                     <div style={{ backgroundColor: 'white', width: '60px', height: '30px'}}></div>
                     <div style={{ backgroundColor: 'white', width: '60px', height: '30px'}}></div>
